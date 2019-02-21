076c040b89bf42b7739e13b72c6e028834771422
Matt-F-Wu <wuhao20@stanford.edu>
2019-02-20 20:49:32 -0800

    example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+/* global firebase */
+
+/* Function to send an email with firebase*/
+
+const baseUrl = 'https://relai-223119.appspot.com/server';
+
+function emailPartner(name, email){
+
+  const argString = [
+      `email=${ email }`,
+      `name=${ name }`
+    ].join('&');
+
+  const actionCodeSettings = {
+    // URL serves as a payload to our mobile app.
+    // URL must be whitelisted in the Firebase Console.
+    url: `${ baseUrl }/register?${ argString }`,
+    // This must be true.
+    handleCodeInApp: true,
+    iOS: {
+      bundleId: 'com.relai.ios',
+    },
+    android: {
+      packageName: 'com.relai.android',
+      installApp: true,
+    },
+    dynamicLinkDomain: 'relai.page.link',
+  };
+  return firebase.auth().sendSignInLinkToEmail(
+      email, actionCodeSettings);
+}
+
 class App extends Component {
+  state = {
+    email: '',
+    name: '',
+  }
+
+  handleChange = (name) => (event) => {
+    this.setState({[name]: event.target.value});
+  }
+
   render() {
+    const { email, name } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div>
+          Email:
+          <input
+            type="email"
+            value={ email }
+            onChange={this.handleChange("email") }
+          />
+        </div>
+        <div>
+          Name:
+          <input
+            type="text"
+            value={ name }
+            onChange={ this.handleChange("name") }
+          />
+        </div>
+        <button onClick={ () => emailPartner(name, email) }>
+          Send Email
+        </button>
       </div>
     );
   }
